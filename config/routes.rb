9b0a84cdcb33feec31f1b4a259849560b00531fc
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-08-20 14:53:56 +0200

    heroku init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
-  get 'home/index'
+Rails.application.routes.draw do  
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
